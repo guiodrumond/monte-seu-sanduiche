22edc5db14ce0ae275f52771caec3599acc23cbb
Guilherme Oliveira Drumond <guilherme.odrumond@sp.senac.br>
2022-08-24 09:54:53 -0300

    Primeiro commit. Ainda trabalhando na estutura básica do front-end

---
+++ src/components/content/index.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import Header from '../header'
+
+class Content extends Component {
+    render () {
+        return (
+            <div className='content'>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Content
---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import './style.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1 className='title'>Monte Seu Sanduíche</h1>
+            </div>            
+        )
+    }
+}
+
+export default Header
